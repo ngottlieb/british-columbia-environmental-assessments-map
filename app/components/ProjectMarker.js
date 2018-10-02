adc393eed2b2a74de4a91c6122909d5e411432ee
Nick Gottlieb <ngottlieb@gmail.com>
2018-10-01 20:47:47 -0700

    add more info to display boxes

--- app/components/ProjectMarker.js
+++ app/components/ProjectMarker.js
@@ -1,6 +1,7 @@
 import { Marker, Popup } from 'react-leaflet';
 import React from 'react';
 import L from 'leaflet';
+import moment from 'moment';
 
 export default class ProjectMarker extends React.Component {
   constructor(props) {
@@ -12,6 +13,16 @@ export default class ProjectMarker extends React.Component {
       <Marker position={[this.props.lat, this.props.lon]} icon={this.projectIcon()}>
         <Popup>
           <h3>{this.props.name}</h3>
+          <dl>
+            <dt>Type</dt>
+            <dd>{this.props.type}</dd>
+            <dt>Decision</dt>
+            <dd>{this.props.eacDecision ? this.props.eacDecision : 'N/A'}</dd>
+            <dt>Decision Date</dt>
+            <dd>{this.props.decisionDate ? moment(this.props.decisionDate).format('MMMM Do YYYY') : 'N/A'}</dd>
+            <dt>Proponent</dt>
+            <dd>{this.props.proponent ? this.props.proponent.name : ''}</dd>
+          </dl>
           <p>{this.props.description}</p>
           <a target="_blank" href={this.projectUrl()}>More Info</a>
         </Popup>
